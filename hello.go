50d6c50894c832519858e1516ba3fc27e97bd2c5
Rogério A. Ferreira <ro.ferreira87sp@gmail.com>
2022-02-26 23:32:33 -0300

    Introdução a controle de fluxo com if, else if e switch

--- hello.go
+++ hello.go
@@ -16,4 +16,25 @@ func main() {
 	fmt.Scanf("%d", &comando)
 
 	fmt.Println("O comando escolhido foi", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do programa")
+	// } else {
+	// 	fmt.Println("Não conheço")
+	// }
+
+	switch comando {
+	case 1 :
+		fmt.Println("Monitorando...")
+	case 2 :
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço")
+	}
 }
