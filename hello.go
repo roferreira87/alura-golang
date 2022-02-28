f93373c099367042e9004d47b6861607aaaad8cf
Rogério A. Ferreira <ro.ferreira87sp@gmail.com>
2022-02-28 12:16:40 -0300

    Inserido pacote site/http, criado lógica de monitoramento do case 1 com script de loop para site alura

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+//import "net/http"
+
+
 
 func main() {
 
@@ -16,27 +23,32 @@ func main() {
 	// }
 
 	exibeIntroducao()
-	exibeMenu()
+	
+	for {
+		exibeMenu()
 
-	comando := leComando()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciaMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço")
+			os.Exit(-1)
+		}
 	}
+	
 }
+
 
 func exibeIntroducao() {
 	nome := "Rogério"
-	versao := 1.1
+	versao := 1.2
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -55,4 +67,17 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 	
 	return comandoLido
+}
+
+func iniciaMonitoramento () {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
+	//fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	}else{
+		fmt.Println("site", site, "esta com problema. Status Code:", resp.StatusCode)
+	}
 }
